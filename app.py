6b36381c866ae5620901c340192b2eff053f7ac9
Junghyun Min <jmin10@jhu.edu>
2021-06-11 08:05:28 +0900

    Update app.py
    Change format to match wrapper

--- app.py
+++ app.py
@@ -30,19 +30,55 @@ def create_app():
 
     @app.route('/api/v1/flair_chunking', methods=['POST'])
     def chunk():
-        if not request.json or not 'message' in request.json:
-            abort(400)
-        entire_message = request.json['message']
+    '''
+    Performs chunking on a list of input sentences and returns chunking result
 
-        responses = {"sentences": []}
+    Input:
+    Chunking objective in JSON format: {
+        "sentence": [
+            {
+                "text": "This is a sentence."
+            },
+            {
+                "text": "This is another sentence."
+            }
+        ]
+    }
+
+    Returns:
+    Response in JSON format: {
+        "sentence": [
+            {
+                "chunk_str": "<This> <is> <a sentence> .",
+                "chunks": [
+                    {
+                        "end_pos": 4
+                        "labels": "[NP (0.9964)]",
+                        "start_pos": 0,
+                        "text": "This"
+                    }, ...
+                ],
+                "text": "This is a sentence ."
+            }
+        ]
+
+    }
+
+    '''
+        if not request.json or not 'sentence' in request.json:
+            abort(400)
+        entire_message = request.json['sentence'] # list under the key "sentence", whose elements are single-value dictionaries {"text": "This is a sentence."}
+
+        responses = {"sentence": []}
 
         for i in range(int(len(entire_message)/5) + 1):
             messages = entire_message[i:min(i+5, len(entire_message))]
-            for msg in messages:
-                if len(msg) > 5000:
+            for message in messages:
+                msg = message["text"]
+                if len(msg) > 5000: # cutoff of length 5000 for input sentences
                     msg = msg[:5000]
-                message = msg.replace("%27", "'")
-                sentence = Sentence(message)
+                msg = msg.replace("%27", "'")
+                sentence = Sentence(msg)
                 try:
                     tagger.predict(sentence)
                 except:
@@ -50,7 +86,7 @@ def create_app():
                     print('Exiting loop...')
                     break
 
-                response = {"text": message}
+                response = {"text": msg}
                 response["chunks"] = sentence.to_dict(tag_type='np')["entities"]
 
                 chunk_str = ""
@@ -67,8 +103,6 @@ def create_app():
 
 
         return jsonify(responses), 200
-
-    return app
 
 if __name__ == "__main__":
     #from waitress import serve
